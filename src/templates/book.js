3038d6fe30f3bea2a762a3db3681f8b90361dbac
henrylatham <henry@henrylatham.me>
2019-05-07 20:38:18 +0200

    book promo video

--- src/templates/book.js
+++ src/templates/book.js
@@ -15,6 +15,7 @@ const Book = ({ data }) => {
     <Layout title={`Book - ${title}`} description={subtitle}>
       <Sidebar />
       <Page title=" ">
+        <iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/owxq7zVWM6I?rel=0&amp;controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <MailForm />
       </Page>
     </Layout>
